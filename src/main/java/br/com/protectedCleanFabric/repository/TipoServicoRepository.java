e22bb634c0644433d4c1bf5327fef2ad25b26053
Lucas Luiz da Silva <lucas.oficial2727@gmail.com>
2024-03-21 11:28:04 -0300

    Criação do tipo de serviço
    Criar isso será importante para cadastrar o pedido dos clientes

---
+++ src/main/java/br/com/protectedCleanFabric/repository/TipoServicoRepository.java
@@ -0,0 +1,9 @@
+package br.com.protectedCleanFabric.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.protectedCleanFabric.entity.TipoServicoEntity;
+
+public interface TipoServicoRepository extends JpaRepository<TipoServicoEntity, Long>{
+
+}
