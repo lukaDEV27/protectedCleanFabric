ac3113e502f6f373b92b0ece1437a28ee5d56986
Lucas Luiz da Silva <lucas.oficial2727@gmail.com>
2024-04-29 14:53:02 -0300

    Update ClienteServiceImpl.java

--- src/main/java/br/com/protectedCleanFabric/service/ClienteServiceImpl.java
+++ src/main/java/br/com/protectedCleanFabric/service/ClienteServiceImpl.java
@@ -18,19 +18,10 @@ public class ClienteServiceImpl implements ClienteService{
 	
 	@Override
 	public String save(ClienteEntity clienteEntity) throws Exception {
-		if (clienteEntity.getNome() == null) {
-			this.mensagem = "Digite o nome do cliente.";
-			throw new Exception("Preencha o nome do cliente.");
-		} else if (clienteEntity.getSobrenome() == null) {
-			this.mensagem = "Digite o sobrenome do cliente.";
-			throw new Exception("Preencha o sobrenome do cliente.");
-		} else if (clienteEntity.getEndereco() == null) {
-			this.mensagem = "Digite o endereço do cliente.";
-			throw new Exception("Preencha o endereço do cliente.");
-		} else {
+		
 			this.mensagem = "Cliente cadastrado com sucesso!";
 			clienteRepository.saveAndFlush(clienteEntity);
-		}
+		
 		return mensagem;
 	}
 
